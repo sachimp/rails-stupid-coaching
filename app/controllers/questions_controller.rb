092642b4f8e688fe9497bcad6138b7debec9da2f
Sachin Gandhi <sachingandhi@live.com>
2022-02-14 13:05:01 +0000

    created 'stupid coaching', my first rails web app which switches between a question and answer page. Your work coach responds depending on what you say to them

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = params[:question]
+    @response = nil
+    if @answer == "I am going to work"
+      @response = "Great!"
+    elsif @answer.include? "?"
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
